c69e0c4b1fb8e7b8a2189b3c5e0aa590918beea9
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2018-06-06 16:44:24 -0700

    Install creep-task

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ErrorMapper } from "ErrorMapper"
 import { hoge } from "test"
+import Tasks from 'creep-tasks'
 
 export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Hello, World`)
